a8c56ffc680bf4442c448daf627de5d3446d87ed
yel_hb <bin.huang@aispeech.com>
2018-05-14 15:04:36 +0800

    Update

---
+++ go_yaml_demo.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"encoding/json"
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	Global struct {
+		LogPath string `yaml:"logPath"`
+		LogName string `yaml:"logName"`
+	} `yaml:"global"`
+
+	KafkaConfig struct {
+		Version    string   `yaml:"version"`
+		BrokerList []string `yaml:"brokerList""`
+	} `yaml:"kafkaConfig"`
+
+	ClusterName string `yaml:"clusterName"`
+
+	LogOps []struct {
+		LogType    string `yaml:"logType"`
+		Expression string `yaml:"expression"`
+		Topic      string `yaml:"topic"`
+	} `yaml:"logOps"`
+}
+
+func main() {
+	var cfg Config
+	cfgFile, err := ioutil.ReadFile("./config.yaml")
+	if err != nil {
+		fmt.Printf("read config file failed: %s\n", err.Error())
+		os.Exit(-1)
+	}
+
+	err = yaml.Unmarshal(cfgFile, &cfg)
+
+	if err != nil {
+		fmt.Printf("parse config failed: %s\n", err.Error())
+		os.Exit(-1)
+	}
+
+	fmt.Printf("%+v\n", cfg)
+
+	cfgJStr, err := json.Marshal(cfg)
+	if err != nil {
+		fmt.Printf("convert config to json string failed: %s\n", err.Error())
+		os.Exit(-1)
+	}
+
+	fmt.Printf("%s\n", cfgJStr)
+}
